0cc0cdca2369355584af0e19b72748258e20249b
mbl1000 <markblewis2008@gmail.com>
2025-09-07 14:42:10 -0700

    @Autowired and Annotated for spring

--- src/main/java/com/marklewis/store/OrderService.java
+++ src/main/java/com/marklewis/store/OrderService.java
@@ -1,9 +1,22 @@
 package com.marklewis.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service //This tells spring to manage types of order service
+//Depending on the purpose we may have @Component, @Service if it is for classes containing
+// business purposes, @Repository if interacting with a database, and @Controller to tell Spring class is a Controller
+// for Http
+//@Component is a general purpose annotation and @Service is an alias for @Component
+
 public class OrderService {
 
     private PaymentService paymentService;
 
+//    @Autowired
+    //In previous spring versions @Autowired was required to tell spring to load OrderService with its dependencies.
+    //However it is no longer needed as long as the class has a single constructor
     public OrderService(PaymentService  paymentService){//Constructor function for class
         //Injecting the PaymentService Object into the OrderService Class
         //Dependency is the parameter for the constructor
