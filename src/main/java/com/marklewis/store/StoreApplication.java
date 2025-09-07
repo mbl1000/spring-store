1e7d83d2cc7e2367bbcf24b7f2384a07e1866a00
mbl1000 <markblewis2008@gmail.com>
2025-09-07 13:44:12 -0700

    Created a setter in order service to set payment service in the main Store Application.  (Constructor Injection is industry standard way to do it)

--- src/main/java/com/marklewis/store/StoreApplication.java
+++ src/main/java/com/marklewis/store/StoreApplication.java
@@ -7,13 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
     public static void main(String[] args) {
+
+        //https://www.youtube.com/watch?v=gJrjgg1KVL4// Link for reference*******************************
+
         //SpringApplication.run(StoreApplication.class, args);
         //Assign a variable with the OrderService class functions and provide Order Service with the chosen dependency
         //Both of these dependencies are implemented by the interface and perform processPayment() because they
         //implement the interface.  Inside the OrderService class is where the code states to use the dependency choice
         //to process the payment
         //var orderService = new OrderService(new StripePaymentService());
-        var orderService = new OrderService(new PayPalPaymentService());
+
+        var orderService = new OrderService();
+        orderService.setPaymentService(new PayPalPaymentService());
         orderService.placeOrder();
     }
 }
