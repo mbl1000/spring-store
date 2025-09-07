52aaa22a9312455c46039153b9868033d4173f03
mbl1000 <markblewis2008@gmail.com>
2025-09-07 13:18:03 -0700

    From the store application an variable is given a new instance of the Order Service class.  The order service class is coded to accept a dependency (Paypall or Stripe payment service) to perform and implement it via the interface.  In this way order service doesn't direclty call Paypall or Stripe so the dependency can be swapped in and out.
    OrderService depends on the abstraction (PaymentService), not the concrete class.
    
    You can inject any class that implements PaymentService—like StripePaymentService, MockPaymentService, etc.
    
    This is the Dependency Inversion Principle in action: high-level modules (OrderService) should not depend on low-level modules (PayPalPaymentService); both should depend on abstractions.

--- src/main/java/com/marklewis/store/StoreApplication.java
+++ src/main/java/com/marklewis/store/StoreApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
+        //SpringApplication.run(StoreApplication.class, args);
+        //Assign a variable with the OrderService class functions and provide Order Service with the chosen dependency
+        //Both of these dependencies are implemented by the interface and perform processPayment() because they
+        //implement the interface.  Inside the OrderService class is where the code states to use the dependency choice
+        //to process the payment
+        //var orderService = new OrderService(new StripePaymentService());
+        var orderService = new OrderService(new PayPalPaymentService());
+        orderService.placeOrder();
     }
-
 }
