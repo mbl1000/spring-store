18ab1757b0f73a433738d6426bbd5bc84c076a76
mbl1000 <markblewis2008@gmail.com>
2025-09-07 14:16:05 -0700

    Introduced Spring Application Context and early beginning to beans.

--- src/main/java/com/marklewis/store/OrderService.java
+++ src/main/java/com/marklewis/store/OrderService.java
@@ -4,13 +4,13 @@ public class OrderService {
 
     private PaymentService paymentService;
 
-//    public OrderService(PaymentService  paymentService){//Constructor function for class
-//        //Injecting the PaymentService Object into the OrderService Class
-//        //Dependency is the parameter for the constructor
-//        //This definition of payment service is assigned the paymentService parameter from the Interface Payment Service
-//        //This is how the interface methods "come into" the Order Service class.
-//        this.paymentService = paymentService;
-//    }
+    public OrderService(PaymentService  paymentService){//Constructor function for class
+        //Injecting the PaymentService Object into the OrderService Class
+        //Dependency is the parameter for the constructor
+        //This definition of payment service is assigned the paymentService parameter from the Interface Payment Service
+        //This is how the interface methods "come into" the Order Service class.
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder(){
         //Now the paymentService variable holds (and implements) the processPayment because it was injected through
@@ -19,7 +19,7 @@ public class OrderService {
 
     }
 
-    public void setPaymentService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public void setPaymentService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 }
--- src/main/java/com/marklewis/store/StoreApplication.java
+++ src/main/java/com/marklewis/store/StoreApplication.java
@@ -2,6 +2,7 @@ package com.marklewis.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
@@ -10,15 +11,23 @@ public class StoreApplication {
 
         //https://www.youtube.com/watch?v=gJrjgg1KVL4// Link for reference*******************************
 
-        //SpringApplication.run(StoreApplication.class, args);
+
+        //run returns a type of application context, our IOC container. The IOC container stores the objects.
+        //It stores the return in a sprint ApplicationContext type.  Here we name the Application Context "context"
+        //The context comes with its own built in methods managed by spring.
+        ApplicationContext context =  SpringApplication.run(StoreApplication.class, args);
+        //A bean is an object in spring that is injected and has its life cycle managed by spring
+        var orderService = context.getBean(OrderService.class);
+
+
         //Assign a variable with the OrderService class functions and provide Order Service with the chosen dependency
         //Both of these dependencies are implemented by the interface and perform processPayment() because they
         //implement the interface.  Inside the OrderService class is where the code states to use the dependency choice
         //to process the payment
         //var orderService = new OrderService(new StripePaymentService());
 
-        var orderService = new OrderService();
-        orderService.setPaymentService(new PayPalPaymentService());
+        //var orderService = new OrderService(new PayPalPaymentService());  Now managed by spring when define
+        // orderservice above
         orderService.placeOrder();
     }
 }
